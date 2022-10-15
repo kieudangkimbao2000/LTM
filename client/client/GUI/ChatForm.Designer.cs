09bbec355a339b3b19f79011370cbd26cb5cd61e
unknown <kieudangkimbao2000@gmail.com>
2022-10-15 10:08:57 +0700

    ___

--- client/client/GUI/ChatForm.Designer.cs
+++ client/client/GUI/ChatForm.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace client
+﻿namespace client.GUI
 {
     partial class ChatForm
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,23 +23,15 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // ChatForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "ChatForm";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.ChatForm_Load);
-            this.ResumeLayout(false);
-
+            this.Text = "ChatForm";
         }
 
         #endregion

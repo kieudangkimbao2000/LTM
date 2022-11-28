df370c43ebbfd4caee90738bbda2321f77488782
disaster253 <65069829+disaster253@users.noreply.github.com>
2022-11-28 16:37:13 +0700

    Add files via upload
    add login form

---
+++ client/client/GUI/LoginForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace client.GUI
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.user = new System.Windows.Forms.Label();
+            this.pwd = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPwd = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // user
+            // 
+            this.user.AutoSize = true;
+            this.user.Location = new System.Drawing.Point(45, 128);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(73, 20);
+            this.user.TabIndex = 1;
+            this.user.Text = "username";
+            // 
+            // pwd
+            // 
+            this.pwd.AutoSize = true;
+            this.pwd.Location = new System.Drawing.Point(45, 167);
+            this.pwd.Name = "pwd";
+            this.pwd.Size = new System.Drawing.Size(72, 20);
+            this.pwd.TabIndex = 2;
+            this.pwd.Text = "password";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(175, 210);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(94, 29);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(152, 125);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(179, 27);
+            this.tbUser.TabIndex = 4;
+            // 
+            // tbPwd
+            // 
+            this.tbPwd.Location = new System.Drawing.Point(152, 167);
+            this.tbPwd.Name = "tbPwd";
+            this.tbPwd.Size = new System.Drawing.Size(179, 27);
+            this.tbPwd.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(175, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 287);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tbPwd);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.pwd);
+            this.Controls.Add(this.user);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label user;
+        private Label pwd;
+        private Button btnLogin;
+        private TextBox tbUser;
+        private TextBox tbPwd;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ client/client/GUI/LoginForm.cs
@@ -0,0 +1,77 @@
+﻿using ContentPackage;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace client.GUI
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+
+            InitializeComponent();
+            Thread thread = new Thread(() => { clientRecive(); });
+            thread.IsBackground = true;
+            thread.Start();
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+            TcpClient client = new TcpClient();
+            client.Connect(iep);
+            Account account = new Account();
+            account.username = tbUser.Text;
+            account.password = tbPwd.Text;
+            Package package = new Package() { kind = 202, content = JsonSerializer.Serialize<Account>(account) };
+            string sendStr = JsonSerializer.Serialize<Package>(package);
+            Program.sw.WriteLine(sendStr);
+            Program.sw.Flush();
+
+        }
+        private void clientRecive()
+        {
+            Package? package = new Package();
+            while (true)
+            {
+                string recvStr = Program.sr.ReadLine();
+                package = JsonSerializer.Deserialize<Package>(recvStr);
+                if (package != null)
+                {
+                    if (package.content != null)
+                    {
+                        switch (package.kind)
+                        {
+                            #region 100
+                            case 100:
+                                MessageBox.Show("Đăng nhập thất bại");
+                                break;
+                            #endregion
+                            #region 101
+                            case 101:
+                                MessageBox.Show("Đăng nhập thành công");
+                                ChatForm chat = new ChatForm();
+                                chat.username = tbUser;
+                                chat.ShowDialog();
+                                break;
+                            #endregion
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
